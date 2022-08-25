43ebe90dffa1f939016d9087575f5e4e116e2c6f
tuan huy <100185063+huynopro102@users.noreply.github.com>
2022-08-26 00:21:04 +0700

    add code

---
+++ app.js
@@ -0,0 +1,42 @@
+var tagname = document.querySelector('.content .name')
+var tagcapital = document.querySelector('.content .capital')
+var taginput = document.querySelector('.search')
+var tagtime = document.querySelector('.time')
+var tagtemperature = document.querySelector('.temperature')
+var tagshortdescription = document.querySelector('.short_description')
+var tagvisibility = document.querySelector('.visibility span')
+var tagshort_nav_description = document.querySelector('.short_nav_description')
+var tagspeed = document.querySelector('.speed span')
+var taghumidity = document.querySelector('.humidity span')
+var tagcontent = document.querySelector('.content')
+console.log(tagcontent)
+taginput.addEventListener('keydown',function(e){
+    if(e.key == 'Enter'){
+     tagnamedescript = e.target.value
+     changeApi()
+    }
+ })
+ var tagnamedescript 
+async function changeApi(){
+    let myapi = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${tagnamedescript}&appid=4dfe500e5d2fcaee597a5551283e33d4&lang=vi`).then(response => response.json()) 
+        console.log(myapi)
+        if(myapi.cod == 200){
+            tagcontent.classList.remove('hidden')
+           
+            tagname.innerHTML = myapi.name
+            tagcapital.innerHTML = myapi.sys.country
+            // chuyển độ k sang độ c -273.15
+            tagtemperature.textContent = Math.round((myapi.main.temp - 273.15))
+            tagshortdescription.innerHTML = myapi.weather[0].main
+            tagvisibility.innerHTML = myapi.visibility + 'm'
+            tagshort_nav_description.innerHTML = myapi.weather[0].description
+            // tim dùng Date trong js
+            tagtime.innerHTML = new Date().toLocaleString()
+            tagspeed.innerHTML = myapi.wind.speed + 'm/s'
+            taghumidity.innerHTML = myapi.main.humidity + '%'
+        }else{
+            tagcontent.classList.add('hidden')
+            alert('no find')
+        }
+}
+changeApi()
